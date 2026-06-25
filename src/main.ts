208b96202186526aedc27bbde1cbd71598860c5c
antoinedmns <antoine.damiens1@orange.fr>
2026-06-25 15:01:42 +0200

    fix: add conflicts when performing a dry run

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { Command } from 'commander';
-import { parser } from './parser.ts';
+import { Command, Option } from 'commander';
+import { parser } from './parser.js';
 const program = new Command();
 
 program
@@ -9,13 +9,14 @@ program
 
 program.command('commit')
     .description('Create a new commit.')
-    .option('--dry-run', 'Perform a dry run without executing any commands.')
-    .option('-r, --retry', 'Retry the last commit.')
-    .option('-c, --copy', 'Copy the commit message to your clipboard instead of committing.')
-    .option('-a, --add', 'Automatically stage all created, deleted or modified files.')
-    .option('-p, --push', 'Automatically push staged files from the local to the remote repository.')
-    .option('-s, --sign', 'Sign your commit.')
-    .option('-l, --length <number>', 'Set the length limit of the message.')
+    .addOption(new Option('--dry-run', 'Perform a dry run without executing any commands.').conflicts(['add', 'push']))
+    .addOption(new Option('-r, --retry', 'Retry the last commit.'))
+    .addOption(new Option('-c, --copy', 'Copy the commit message to your clipboard instead of committing.'))
+    .addOption(new Option('-a, --add', 'Automatically stage all created, deleted or modified files.'))
+    .addOption(new Option('-p, --push', 'Automatically push staged files from the local to the remote repository.'))
+    .addOption(new Option('-s, --sign', 'Sign your commit.'))
+    .addOption(new Option('-d, --debug', 'Display the debug output.'))
+    .addOption(new Option('-l, --length <number>', 'Set the length limit of the message.'))
     .action((options) => {
         parser(options)
     })
