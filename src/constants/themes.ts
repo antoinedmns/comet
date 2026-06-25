2a1afcd83c6820b653544afd724472f2992b1b3f
antoinedmns <antoine.damiens1@orange.fr>
2026-06-25 16:02:05 +0200

    feat: adding a logger

--- src/constants/themes.ts
+++ src/constants/themes.ts
@@ -1,4 +1,5 @@
 import chalk from 'chalk';
+import type { ExecException } from 'child_process';
 
 export const DefaultTheme = {
     prefix: chalk.yellow('?'),
@@ -9,3 +10,7 @@ export const DefaultTheme = {
         error: (message: string) => chalk.red.bold(`! ${message}`)
     }
 } as const;
+
+export function logSuccess(message: string): void { console.log(chalk.greenBright(`\n${message}`)); }
+export function logError(message: string): void { console.log(chalk.red.bold(`\n! ${message}`)); }
+export function logDebug(message: ExecException | string): void { console.log(chalk.gray(`${message}`)); }
